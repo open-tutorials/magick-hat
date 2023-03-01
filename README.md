faab32fd6bdb5d481dba01fca3f9af59394797d8
breslavsky <breslavsky.anton@gmail.com>
2023-03-01 13:33:30 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# magick-hat
-Your magick assets converter
+# Magic Hat
+
+Fast assets converter for your tutorials.
+
+
+# Using
+
+- [ ] Install dependencies:
+
+```bash
+brew install imagemagick
+brew install ffmpeg
+npm ci
+```
+
+- [ ] Run application:
+
+```bash
+npx gulp
+```
+
+- [ ] Move source file to any target folder:
+
+<img title="How to use magic hat" width="866" height="624" src="howto.gif">
+
+- [ ] Run web server if your need:
+
+```bash
+npm start
+open http://localhost:3000
+```
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+const { watch } = require('gulp');
+const path = require('node:path');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const templates = {
+    webp: (file, width, height) =>
+        `<img class="cornered" title="?"\n    width="${width}" height="${height}" src="${file}">`,
+    gif: (file, width, height) =>
+        `<img class="cornered" title="?"\n    width="${width}" height="${height}" src="${file}">`,
+    mp4: (file, width, height) =>
+        `<video width="600px" controls>\n    <source src="${file}" type="video/mp4">\n</video>`
+};
+
+const commands = {
+    identify: file => `identify '${file}'`,
+    toWebP: (src, dst) => `convert '${src}' '${dst}'`,
+    toGif: (src, dst) => [
+        `ffmpeg -i '${src}'`,
+        '-vf "fps=10,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer"',
+        `'${dst}'`
+    ].join(' '),
+    toMp4: (src, dst) => `ffmpeg -i '${src}' -vcodec h264 -acodec mp2 '${dst}'`
+};
+
+const folders = {
+    webp: 'watch/2webp',
+    jpeg: 'watch/2jpeg',
+    gif: 'watch/2gif',
+    mp4: 'watch/2mp4',
+    backup: 'watch/backup'
+};
+
+for (const dir of [folders.webp, folders.jpeg, folders.gif, folders.mp4, folders.backup]) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+}
+
+function identify(file, template) {
+    const identify = exec(commands.identify(file));
+    identify.stdout.on('data', data => {
+        const [, w, h] = data.match(/(\d+)x(\d+)/);
+        const width = Math.floor(parseInt(w) / 2);
+        const height = Math.floor(parseInt(h) / 2);
+        console.log(template(file, width, height));
+    });
+}
+
+function toWebP(png) {
+    const { name, base } = path.parse(png);
+    const webp = path.join('watch', name + '.webp');
+    console.log('converting', png, 'to', webp);
+
+    const cmd = commands.toWebP(png, webp);
+    console.log(cmd);
+
+    const convert = exec(cmd);
+    convert.addListener('close', code => {
+        console.log('done', code);
+        if (code === 0) {
+            try {
+                fs.renameSync(png, path.join(folders.backup, base));
+            } catch (e) { }
+            identify(png, templates.webp);
+        }
+    });
+}
+
+function toGif(mov) {
+    const { name, base } = path.parse(mov);
+    const gif = path.join('watch', name + '.gif');
+
+    const cmd = commands.toGif(mov, gif);
+    console.log(cmd);
+
+    const ffmpeg = exec(cmd);
+    ffmpeg.stderr.on('data', data => console.log(data));
+    ffmpeg.addListener('close', code => {
+        console.log('done', code);
+        if (code === 0) {
+            try {
+                fs.renameSync(png, path.join(folders.backup, base));
+            } catch (e) { }
+            identify(gif, templates.gif);
+        }
+    });
+}
+
+function toMp4(mov) {
+    const { name, base } = path.parse(mov);
+    const mp4 = path.join('watch', name + '.mp4');
+
+    const cmd = commands.toMp4(mov, mp4);
+    console.log(cmd);
+
+    const ffmpeg = exec(cmd);
+    ffmpeg.stderr.on('data', data => console.log(data));
+    ffmpeg.addListener('close', code => {
+        console.log('done', code);
+        if (code === 0) {
+            try {
+                fs.renameSync(png, path.join(folders.backup, base));
+            } catch (e) { }
+            identify(gif, templates.mp4);
+        }
+    });
+}
+
+exec('open watch');
+
+exports.default = function () {
+    watch(['watch/2webp/*.png']).on('add', (file) => {
+        console.log('added file', file);
+        toWebP(file);
+    });
+    watch(['watch/2gif/*.mov']).on('add', (file) => {
+        console.log('added file', file)
+        toGif(file);
+    });
+    watch(['watch/2mp4/*.mov']).on('add', (file) => {
+        console.log('added file', file)
+        toMp4(file);
+    });
+};
