9063b43534ec0866c52059c343e191478a0bdf45
breslavsky <breslavsky.anton@gmail.com>
2023-03-05 14:23:35 +0100

    Add webm converter

--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,41 @@ const templates = {
     gif: (file, width, height) =>
         `<img class="cornered" title="?"\n    width="${width}" height="${height}" src="${file}">`,
     mp4: (file, width, height) =>
-        `<video width="600px" controls>\n    <source src="${file}" type="video/mp4">\n</video>`
+        `<video width="600px" controls>\n    <source src="${file}" type="video/mp4">\n</video>`,
+    webm: (file, width, height) =>
+        `<video class="how-to" width="${width}" height="${height}" loop autoplay muted>\n    <source src="${file}" type="video/webm">\n</video>`
 };
 
 const commands = {
     identify: file => `identify '${file}'`,
-    toWebP: (src, dst) => `convert '${src}' '${dst}'`,
-    toJpeg: (src, dst) => `convert '${src}' -quality 80 '${dst}'`,
+    toWebP: (src, dst) => [
+        `convert '${src}'`,
+        `'${dst}'`
+    ].join(' '),
+    toJpeg: (src, dst) => [
+        `convert '${src}'`,
+        '-quality 80',
+        `'${dst}'`
+    ].join(' '),
     toGif: (src, dst) => [
         `ffmpeg -i '${src}'`,
         '-vf "fps=10,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer"',
         `'${dst}'`
     ].join(' '),
-    toMp4: (src, dst) => `ffmpeg -i '${src}' -vcodec h264 -acodec mp2 '${dst}'`
+    toMp4: (src, dst) => [
+        `ffmpeg -i '${src}'`,
+        '-vcodec h264',
+        '-acodec mp2',
+        `'${dst}'`
+    ].join(' '),
+    toWebM: (src, dst) => [
+        `ffmpeg -i '${src}'`,
+        '-vcodec libvpx-vp9',
+        '-b:v 1M',
+        '-crf 33',
+        '-acodec libvorbis',
+        `'${dst}'`
+    ].join(' ')
 };
 
 const folders = {
@@ -31,10 +53,11 @@ const folders = {
     jpeg: 'watch/2jpeg',
     gif: 'watch/2gif',
     mp4: 'watch/2mp4',
+    webm: 'watch/2webm',
     backup: 'watch/backup'
 };
 
-for (const dir of [folders.webp, folders.jpeg, folders.gif, folders.mp4, folders.backup]) {
+for (const dir of [folders.webp, folders.jpeg, folders.gif, folders.mp4, folders.webm, folders.backup]) {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
     }
@@ -42,11 +65,15 @@ for (const dir of [folders.webp, folders.jpeg, folders.gif, folders.mp4, folders
 
 function identify(file, template) {
     const identify = exec(commands.identify(file));
-    identify.stdout.on('data', data => {
-        const [, w, h] = data.match(/(\d+)x(\d+)/);
-        const width = Math.floor(parseInt(w) / 2);
-        const height = Math.floor(parseInt(h) / 2);
-        console.log(template(file, width, height));
+    let stdout = '';
+    identify.stdout.on('data', data => stdout += data);
+    identify.addListener('close', code => {
+        if (code === 0) {
+            const [, w, h] = stdout.match(/(\d+)x(\d+)/);
+            const width = Math.floor(parseInt(w) / 2);
+            const height = Math.floor(parseInt(h) / 2);
+            console.log(template(file, width, height));
+        }
     });
 }
 
@@ -130,6 +157,26 @@ function toMp4(mov) {
     });
 }
 
+function toWebM(mov) {
+    const { name, base } = path.parse(mov);
+    const webm = path.join('watch', name + '.webm');
+
+    const cmd = commands.toWebM(mov, webm);
+    console.log(cmd);
+
+    const ffmpeg = exec(cmd);
+    ffmpeg.stderr.on('data', data => console.log(data));
+    ffmpeg.addListener('close', code => {
+        console.log('done', code);
+        if (code === 0) {
+            try {
+                fs.renameSync(mov, path.join(folders.backup, base));
+            } catch (e) { }
+            identify(webm, templates.webm);
+        }
+    });
+}
+
 exec('open watch');
 
 exports.default = function () {
@@ -148,5 +195,9 @@ exports.default = function () {
     watch(['watch/2mp4/*.mov']).on('add', (file) => {
         console.log('added file', file)
         toMp4(file);
+    });
+    watch(['watch/2webm/*.mov']).on('add', (file) => {
+        console.log('added file', file)
+        toWebM(file);
     });
 };
